c0db283ae5da31323fc083e6e9ea12eaeff4390b
TrustedMercury <kevin.jt2007@gmail.com>
2020-07-21 13:10:05 -0400

    Added website

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// TailwindCSS Framework - Initialization
+
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sweetBlack: "#17171A",
+        shinyGray: "#1D1E23",
+        beautyPurple: "#9F72DA",
+        blurple: "#7289DA"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
